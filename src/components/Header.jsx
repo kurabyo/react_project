7b5804875ca5fbeaa7c595a367a5e0673d986fd0
kurabyo <bladvazovski@gmail.com>
2022-12-16 16:25:20 +0200

    grid
    adding basic stucture

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+    return (
+      <header className="header__app">
+        <img
+          className="img__logo"
+          alt="logo"
+          src="https://icon-library.com/images/20x20-icon/20x20-icon-28.jpg"
+        />
+      </header>
+    );
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+const Profile = () => {
+    return (
+      <div className="main__app">
+        <img
+          className="profile__img"
+          alt="profile-img"
+          src="https://cdn.shazoo.ru/555088_gQaGFvQZlY_berserk.jpg"
+        />
+        <div>ava + description</div>
+        <div>
+          my posts
+          <div>new post</div>
+          <div>
+            <div>post 1</div>
+            <div>post 2</div>
+            <div>post 3</div>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Profile
